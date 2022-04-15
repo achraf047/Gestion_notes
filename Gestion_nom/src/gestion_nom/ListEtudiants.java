1e5105ec6cbd2658e0623e0c8a45c82623d01bb1
achraf047 <achraf.laghlab2018@gmail.com>
2022-04-15 15:51:13 +0000

    redeifinition de la methode add

--- Gestion_nom/src/gestion_nom/ListEtudiants.java
+++ Gestion_nom/src/gestion_nom/ListEtudiants.java
@@ -3,5 +3,13 @@ package gestion_nom;
 import java.util.ArrayList;
 
 public class ListEtudiants extends ArrayList<Etudiant> {
+	@Override
+	public boolean add(Etudiant e) {
+		if (this.contains(e))
+			return false;
+		// TODO Auto-generated method stub
+		else
+			return super.add(e);
+	}
 
 }
