18d10fb3c7cc410a620bf89d4c405124a468e139
achraf047 <achraf.laghlab2018@gmail.com>
2022-04-15 15:06:18 +0000

    redefintion de la fonction equals

--- Gestion_nom/src/gestion_nom/Etudiant.java
+++ Gestion_nom/src/gestion_nom/Etudiant.java
@@ -1,19 +1,27 @@
 package gestion_nom;
 
+import aiac.gi18.java.complexe.Complexe;
+
 public class Etudiant {
 	private int id;
 	private String nom;
 	private Double note;
-	
-	public  Etudiant(int id , String nom) {
+
+	public Etudiant(int id, String nom) {
 		this.id = id;
 		this.nom = nom;
 		this.note = 10.0;
-   }
-	public String toString()
-	{
-		return this.nom+ " : "+this.note;
 	}
 
+	public String toString() {
+		return this.nom + " : " + this.note;
+	}
+
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Etudiant)) {
+			return false;
+		}
+		return ((Etudiant) obj).id == this.id;
+	}
 
 }
